aa40c10acd12cc5e3cb9a278e818c3e1ff068a76
Stanislav Tkachenko <post4stas.t@gmail.com>
2024-12-24 14:37:08 +0100

    Initial commit.

---
+++ src/main/java/org/sandw/propertymanagementapi/PropertyManagementApiApplication.java
@@ -0,0 +1,13 @@
+package org.sandw.propertymanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropertyManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropertyManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sandw/propertymanagementapi/PropertyManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sandw.propertymanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertyManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
